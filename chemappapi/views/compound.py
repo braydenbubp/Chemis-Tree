1665cbb7e1197d5f6761bfbf2ae1c4a663e9fdb5
braydenbubp <braydenbubp1@gmail.com>
2024-08-24 12:18:06 -0400

    full crud for compounds working, including api

--- chemappapi/views/compound.py
+++ chemappapi/views/compound.py
@@ -15,66 +15,96 @@ class CompoundSerializer(serializers.ModelSerializer):
 class CompoundView(ViewSet):
     
     def retrieve(self, request, pk):
-        compound = Compound.objects.get(pk = compound.cid)
+        compound = Compound.objects.get(pk = pk)
 
         serializer = CompoundSerializer(compound)
         return Response(serializer.data)
       
     def list(self, request):
         compounds = Compound.objects.all()
-        serializer = CompoundSerializer(compounds)
-        return Response(serializer.data, many=True)
+        serializer = CompoundSerializer(compounds, many=True)
+        return Response(serializer.data)
     
     # not sure if i need this now, or need to do it in a different way  
-    def create(self, request):
-        user = User.objects.get(uid=request.data["uid"])
+    # def create(self, request):
+    #     user = User.objects.get(uid=request.data["uid"])
         
-        compound = Compound.objects.create(
-            user = user,
-            common_name = request.data["common_name"],
-            formula = request.data["formula"],
-            smiles = request.data["smiles"],
-            molecular_weight = request.data["molecular_weight"],
-            chemspider_id = request.data["chemspider_id"],
-            two_d_model = request.data["two_d_model"],
-        )
+    #     compound = Compound.objects.create(
+    #         user = user,
+    #         common_name = request.data["common_name"],
+    #         formula = request.data["formula"],
+    #         smiles = request.data["smiles"],
+    #         molecular_weight = request.data["molecular_weight"],
+    #         chemspider_id = request.data["chemspider_id"],
+    #         two_d_model = request.data["two_d_model"],
+    #     )
         
-        for element_id in request.data["elements"]:
-            element = Element.objects.get(pk=element_id)
-            CompoundElement.objects.create(
-                compound = compound,
-                element = element
-            )
+    #     for element_id in request.data["elements"]:
+    #         element = Element.objects.get(pk=element_id)
+    #         CompoundElement.objects.create(
+    #             compound = compound,
+    #             element = element
+    #         )
         
-        serializer = CompoundSerializer(compound)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     serializer = CompoundSerializer(compound)
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
       
     # to be fixed  
     def update(self, request, pk):
-        compound = Compound.objects.get(pk=pk)
-                
-        compound.common_name = request.data["common_name"],
-        compound.formula = request.data["formula"],
-        compound.smiles = request.data["smiles"],
-        compound.molecular_weight = request.data["molecular_weight"],
-        compound.chemspider_id = request.data["chemspider_id"],
-        compound.two_d_model = request.data["two_d_model"],
-        
-        compound_elements = CompoundElement.objects.filter(compound_id = compound.id)
-        for element in compound_elements:
-            element.delete()
+        try:
+            compound = Compound.objects.get(pk=pk)
+            user = request.data.get("user")
+            include_elements = request.data.get("includeElements", [])
+            print(include_elements)
             
-        compound.save()
+            compound_search = "".join(include_elements)
+            # print(compound_search)
+            results = pcp.get_compounds(compound_search, "formula")
+            # print(results)
 
-        for element_id in request.data["elements"]:
-            element = Element.objects.get(pk=element_id)
-            CompoundElement.objects.create(
-                compound = compound,
-                element = element
-            )
-        
-        serializer = CompoundSerializer(compound)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            if results:
+                pubchem_compound = results[0]
+                # print("PubChem data:", {
+                #     "molecular_formula": pubchem_compound.molecular_formula,
+                #     "iupac_name": pubchem_compound.iupac_name,
+                #     "molecular_weight": pubchem_compound.molecular_weight,
+                #     "cid": pubchem_compound.cid,
+                #     "bonds": pubchem_compound.bonds,
+                #     "synonyms": pubchem_compound.synonyms
+                # })
+
+                compound.molecular_formula = pubchem_compound.molecular_formula
+                compound.iupac_name = pubchem_compound.iupac_name or "None"
+                compound.molecular_weight = pubchem_compound.molecular_weight
+                compound.cid = pubchem_compound.cid
+                compound.bonds = [{'aid1': bond.aid1, 'aid2': bond.aid2, 'order': bond.order} for bond in pubchem_compound.bonds]
+                compound.synonyms = pubchem_compound.synonyms[:10] if pubchem_compound.synonyms else []
+                
+                compound_elements = CompoundElement.objects.filter(compound_id = compound.id)
+                for element in compound_elements:
+                    element.delete()
+                
+                compound.save()
+                
+                for element_symbol in request.data["includeElements"]:
+                    try:
+                        element = Element.objects.get(symbol = element_symbol)
+                        CompoundElement.objects.create(
+                            compound = compound,
+                            element = element
+                        )
+                    except Element.DoesNotExist:
+                        print(f"element {element_symbol} DNE")
+                        
+                serializer = CompoundSerializer(compound)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response("compound DNE", status=status.HTTP_404_NOT_FOUND)
+
+        except Compound.DoesNotExist:
+            return Response("compound DNE", status=status.HTTP_404_NOT_FOUND)
+                
+
       
     def destroy(self, request, pk):
         compound = Compound.objects.get(pk=pk)
@@ -86,16 +116,15 @@ class CompoundView(ViewSet):
     @action(detail=False, methods=['post'], url_path='get_compound_by_element')
     def get_compound_by_element(self, request):
             include_elements = request.data["includeElements"]
-            print(include_elements)
+            # print(include_elements)
 
             compound_search = "".join(include_elements)
-            print(compound_search)
+            # print(compound_search)
             results = pcp.get_compounds(compound_search, "formula")
-            print(results)
+            # print(results)
             
             if results:
                 pubchem_compound = results[0]
-                print(results[0].iupac_name)               
                 user = User.objects.get(pk = request.data["user"])
                 compound = Compound.objects.create(
                     user = user,
@@ -104,11 +133,24 @@ class CompoundView(ViewSet):
                     molecular_weight = pubchem_compound.molecular_weight,
                     cid = pubchem_compound.cid,
                     bonds = [{'aid1': bond.aid1, 'aid2': bond.aid2, 'order': bond.order} for bond in pubchem_compound.bonds],
-                    synonyms = pubchem_compound.synonyms
+                    synonyms = pubchem_compound.synonyms[:10] if pubchem_compound.synonyms else [],
                 )
+                
+                for element_symbol in request.data["includeElements"]:
+                    try:
+                        element = Element.objects.get(symbol = element_symbol)
+                        CompoundElement.objects.create(
+                            compound = compound,
+                            element = element
+                        )
+                    except Element.DoesNotExist:
+                        print(f"element {element_symbol} DNE")
                 
                 serializer = CompoundSerializer(compound)
                 
                 return Response(serializer.data, status=status.HTTP_200_OK)
             else:
                 return Response({"message": "No compound found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+            
