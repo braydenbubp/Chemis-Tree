e27e5c6f121c6618636a480de661e40913e6e1b8
braydenbubp <braydenbubp1@gmail.com>
2024-09-14 13:16:24 -0400

    deploy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project_name',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'rdkit-pypi==2024.3.5',
+    ],
+)
