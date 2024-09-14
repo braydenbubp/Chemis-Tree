8f3e499f1d43d20d2e9e58e3557598814d574eb3
braydenbubp <braydenbubp1@gmail.com>
2024-09-14 14:31:31 -0400

    deploy again

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
-    name='chemapp',
-    version='1.0',
-    packages=find_packages(),
-    install_requires=[
-        'rdkit==2024.3.5',
-    ],
-)
+# setup(
+#     name='chemapp',
+#     version='1.0',
+#     packages=find_packages(),
+#     install_requires=[
+#         'rdkit==2024.3.5',
+#     ],
+# )
